9506e8af5fe8c98b2991e142073bd4c35a78317e
Ilirjan Prevazi <ilirprevazi@gmail.com>
2025-06-14 00:33:21 +0200

    Fix line endings and update 2 run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-cd /home/azureuser/weather_app
+
+cd /opt/weather_app
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+
+export PYTHONPATH=/opt/weather_app
 python app/main.py
